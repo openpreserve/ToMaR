1959621fda47549db461993a530f406f346390dc
Matthias Rella <rella@myrho.net>
2014-02-11 09:00:03 +0100

    wrap cmd in sh -c

--- src/main/java/eu/scape_project/pt/proc/CmdLineToolProcessor.java
+++ src/main/java/eu/scape_project/pt/proc/CmdLineToolProcessor.java
@@ -51,10 +51,11 @@ public class CmdLineToolProcessor extends ToolProcessor {
         }
 
         String strCmd = replaceAll(this.operation.getCommand(), allInputs);
-
         LOG.debug("strCmd = " + strCmd );
 
-        proc = Runtime.getRuntime().exec(strCmd);
+        String[] cmd = {"sh", "-c", strCmd};
+
+        proc = Runtime.getRuntime().exec(cmd);
 
         this.setStdIn(proc.getOutputStream());
         this.setStdOut(proc.getInputStream());
