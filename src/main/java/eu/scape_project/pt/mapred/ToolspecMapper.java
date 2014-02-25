a111709cf65406b8aadbfae1426cdb34118bbbbf
Peter Kofler <peter.kofler@code-cop.org>
2014-02-25 22:28:17 +0100

    refactor to remove duplication and extract some methods out of Mapper.map

--- src/main/java/eu/scape_project/pt/mapred/ToolspecMapper.java
+++ src/main/java/eu/scape_project/pt/mapred/ToolspecMapper.java
@@ -38,7 +38,8 @@ import org.apache.hadoop.mapreduce.Mapper;
  */
 public class ToolspecMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
 
-    private static Log LOG = LogFactory.getLog(ToolspecMapper.class);
+    private final Log LOG = LogFactory.getLog(getClass());
+    private static final String SEP = " ";
 
     private CmdLineParser parser;
     private Repository repo;
@@ -78,7 +79,6 @@ public class ToolspecMapper extends Mapper<LongWritable, Text, LongWritable, Tex
     public void map(LongWritable key, Text value, Context context ) throws IOException {
         LOG.info("Mapper.map key:" + key.toString() + " value:" + value.toString());
 
-        String sep = " ";
         Text text = null;
         try {
             // parse input line for stdin/out file refs and tool/action commands
@@ -88,13 +88,11 @@ public class ToolspecMapper extends Mapper<LongWritable, Text, LongWritable, Tex
             final String strStdinFile = parser.getStdinFile();
             final String strStdoutFile = parser.getStdoutFile();
 
-            ToolProcessor firstProcessor = null;
+            Processor firstProcessor = null;
             ToolProcessor lastProcessor = null; 
 
-            Map<String, String>[] mapInputFileParameters = new HashMap[commands.length];
             Map<String, String>[] mapOutputFileParameters = new HashMap[commands.length];
 
-            String workingDir = System.getProperty("user.dir");
             for(int c = 0; c < commands.length; c++ ) {
                 Command command = commands[c];
 
@@ -112,42 +110,28 @@ public class ToolspecMapper extends Mapper<LongWritable, Text, LongWritable, Tex
                 lastProcessor.initialize();
 
                 lastProcessor.setParameters(command.getPairs());
-                lastProcessor.setWorkingDir(workingDir);
+                lastProcessor.setWorkingDir(workingDir());
 
                 // get parameters accepted by the lastProcessor.
-                mapInputFileParameters[c] = lastProcessor.getInputFileParameters(); 
+                Map<String, String> mapInputFileParameters = lastProcessor.getInputFileParameters(); 
                 mapOutputFileParameters[c] = lastProcessor.getOutputFileParameters(); 
 
                 // copy parameters to temporal map
                 Map<String, String> mapTempInputFileParameters = 
-                    new HashMap<String, String>(mapInputFileParameters[c]);
-                Map<String, String> mapTempOutputFileParameters = 
-                    new HashMap<String, String>(mapOutputFileParameters[c]);
+                    new HashMap<String, String>(mapInputFileParameters);
 
                 // localize parameters
-                for( Entry<String, String> entry : mapInputFileParameters[c].entrySet()) {
+                for( Entry<String, String> entry : mapInputFileParameters.entrySet()) {
                     LOG.debug("input = " + entry.getValue());
-                    String[] remoteFileRefs = entry.getValue().split(sep);
-                    String localFileRefs = "";
-                    for( int i = 0; i < remoteFileRefs.length; i++ ){
-                        Filer filer = Filer.create(remoteFileRefs[i]);
-                        filer.setWorkingDir(workingDir);
-                        filer.localize();
-                        localFileRefs = localFileRefs + sep + filer.getRelativeFileRef();
-                    }
+                    String localFileRefs = localiseFileRefs(entry.getValue());
                     mapTempInputFileParameters.put( entry.getKey(), localFileRefs.substring(1));
                 }
 
+                Map<String, String> mapTempOutputFileParameters = 
+                        new HashMap<String, String>(mapOutputFileParameters[c]);
                 for( Entry<String, String> entry : mapOutputFileParameters[c].entrySet()) {
                     LOG.debug("output = " + entry.getValue());
-                    String[] remoteFileRefs = entry.getValue().split(sep);
-                    String localFileRefs = "";
-                    for( int i = 0; i < remoteFileRefs.length; i++ ){
-                        Filer filer = Filer.create(entry.getValue());
-                        filer.setWorkingDir(workingDir);
-                        filer.localize();
-                        localFileRefs = localFileRefs + sep + filer.getRelativeFileRef();
-                    }
+                    String localFileRefs = localiseFileRefs(entry.getValue());
                     mapTempOutputFileParameters.put( entry.getKey(), localFileRefs.substring(1));
                 }
 
@@ -166,55 +150,98 @@ public class ToolspecMapper extends Mapper<LongWritable, Text, LongWritable, Tex
             }
 
             // Processors for stdin and stdout
-            StreamProcessor streamProcessorIn = null;
-            if( strStdinFile != null ) {
-                InputStream iStdin = Filer.create(strStdinFile).getInputStream();
-                streamProcessorIn = new StreamProcessor(iStdin);
-            }
-
-            OutputStream oStdout = null;
-            if( strStdoutFile != null ) 
-                oStdout = Filer.create(strStdoutFile).getOutputStream();
-            else // default: output to bytestream
-                oStdout = new ByteArrayOutputStream();
-
-            StreamProcessor streamProcessorOut = new StreamProcessor(oStdout);
-            lastProcessor.next(streamProcessorOut);
-
+            StreamProcessor streamProcessorIn = createStreamProcessorIn(strStdinFile);
             if( streamProcessorIn != null ) {
                 streamProcessorIn.next(firstProcessor);
-                streamProcessorIn.execute();
-            } else
-                firstProcessor.execute();
+                firstProcessor = streamProcessorIn;
+            } 
 
+            OutputStream oStdout = createStdOut(strStdoutFile);
+            StreamProcessor streamProcessorOut = new StreamProcessor(oStdout);
+            lastProcessor.next(streamProcessorOut);
+            
+            firstProcessor.execute();
 
-            // delocalize output parameters
-            for(int i = 0; i < mapOutputFileParameters.length; i++ ) 
-                for( String strFile : mapOutputFileParameters[i].values())
-                {
-                    String[] localFileRefs = strFile.split(sep);
-                    for( int j = 0; j < localFileRefs.length; j++ ){
-                        Filer filer = Filer.create(localFileRefs[j]);
-                        filer.setWorkingDir(workingDir);
-                        filer.delocalize();
-                    }
-                }
+            delocalizeOutputParameters(mapOutputFileParameters);
 
-                if( oStdout instanceof ByteArrayOutputStream )
-                    text = new Text( ((ByteArrayOutputStream)oStdout).toByteArray() );
-                else
-                    text = new Text( strStdoutFile );
+            text = convertToResult(oStdout, strStdoutFile);
 
         } catch (Exception ex) {
             LOG.error(ex);
-            text = new Text( "ERROR: " + ex.getMessage() );
+            text = convertToResult(ex);
         } finally {
-            try {
-                context.write( key, text);
-            } catch (InterruptedException ex) {
-                LOG.error(ex);
-                throw new IOException(ex);
+            writeMappingResult(key, text, context);
+        }
+    }
+
+    private String localiseFileRefs(String localFile) throws IOException {
+        String[] remoteFileRefs = localFile.split(SEP);
+        String localFileRefs = "";
+        String workingDir = workingDir();
+        for( int i = 0; i < remoteFileRefs.length; i++ ){
+            Filer filer = Filer.create(remoteFileRefs[i]);
+            filer.setWorkingDir(workingDir);
+            filer.localize();
+            localFileRefs = localFileRefs + SEP + filer.getRelativeFileRef();
+        }
+        return localFileRefs;
+    }
+
+    private Text convertToResult(OutputStream oStdout, final String strStdoutFile) {
+        if( oStdout instanceof ByteArrayOutputStream )
+            return  new Text( ((ByteArrayOutputStream)oStdout).toByteArray() );
+        return new Text( strStdoutFile );
+    }
+
+    private Text convertToResult(Exception ex) {
+        return new Text( "ERROR: " + ex.getMessage() );
+    }
+
+    private OutputStream createStdOut(final String strStdoutFile) throws IOException {
+        if( strStdoutFile != null ) 
+            return Filer.create(strStdoutFile).getOutputStream();
+        // default: output to bytestream
+        return new ByteArrayOutputStream();
+    }
+
+    private StreamProcessor createStreamProcessorIn(final String strStdinFile) throws IOException {
+        if( strStdinFile != null ) {
+            InputStream iStdin = Filer.create(strStdinFile).getInputStream();
+            return new StreamProcessor(iStdin);
+        }
+        return null;
+    }
+
+    private void delocalizeOutputParameters(Map<String, String>[] mapOutputFileParameters) throws IOException {
+        for(int i = 0; i < mapOutputFileParameters.length; i++ ) {
+            Map<String, String> outputFileParameters = mapOutputFileParameters[i];
+            delocalizeOutputParameters(outputFileParameters);
+        }
+    }
+
+    private void delocalizeOutputParameters(Map<String, String> outputFileParameters) throws IOException {
+        String workingDir = workingDir();
+        for( String strFile : outputFileParameters.values())
+        {
+            String[] localFileRefs = strFile.split(SEP);
+            for( int j = 0; j < localFileRefs.length; j++ ){
+                Filer filer = Filer.create(localFileRefs[j]);
+                filer.setWorkingDir(workingDir);
+                filer.delocalize();
             }
+        }
+    }
+
+    private String workingDir() {
+        return System.getProperty("user.dir");
+    }
+
+    private void writeMappingResult(LongWritable key, Text text, Context context) throws IOException {
+        try {
+            context.write( key, text);
+        } catch (InterruptedException ex) {
+            LOG.error(ex);
+            throw new IOException(ex);
         }
     }
 }
