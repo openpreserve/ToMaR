5d94f4234a2102958eb30f242fc5fb3ac60ce837
Matthias Rella <rella@myrho.net>
2014-06-10 11:31:08 +0200

    cleanup and streamlining of ControlFileInputFormat
    removed debug messages, adding comments

--- src/main/java/eu/scape_project/pt/mapred/input/ControlFileInputFormat.java
+++ src/main/java/eu/scape_project/pt/mapred/input/ControlFileInputFormat.java
@@ -27,6 +27,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
 import org.apache.hadoop.conf.Configuration;
@@ -36,31 +38,22 @@ import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputSplit;
-import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.JobContext;
-import org.apache.hadoop.mapreduce.RecordReader;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.LineRecordReader;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
 import org.apache.hadoop.util.LineReader;
 
+import eu.scape_project.pt.proc.ToolProcessor;
+import eu.scape_project.pt.repo.Repository;
+import eu.scape_project.pt.repo.ToolRepository;
 import eu.scape_project.pt.tool.Operation;
 import eu.scape_project.pt.tool.Tool;
 import eu.scape_project.pt.util.CmdLineParser;
 import eu.scape_project.pt.util.Command;
 import eu.scape_project.pt.util.PipedArgsParser;
 import eu.scape_project.pt.util.PropertyNames;
-import eu.scape_project.pt.proc.ToolProcessor;
-import eu.scape_project.pt.repo.Repository;
-import eu.scape_project.pt.repo.ToolRepository;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 /**
  * ControlFileInputFormat for a control file with references to files on HDFS.
@@ -72,58 +65,60 @@ import org.apache.commons.logging.LogFactory;
  * hash md5 --input="hdfs://path/to/file"
  *
  * Hadoop's NLineInputFormat would split the control file so that each map task
- * get N such lines. However, as for data locality it is important that the line gets
- * processed by a node which actually hosts the referenced input file. As 
- * NLineInputFormat is agnostic of the input file reference it will spill the map task
- * to any node arbitrarily. 
+ * gets N such lines. However, as NLineInputFormat is agnostic of the block locations of 
+ * the input file reference it will spill the map task  to any node arbitrarily.
  *
- * ControlFileInputFormat extends NLineInputformat
+ * ControlFileInputFormat extends NLineInputformat and adds logic for 
+ * <ol>
+ * <li>resolving a control line's toolspec and input file descriptors contained
+ * <li>retrieving and mapping of block locations to control lines
+ * <li>reordering and balancing control lines by their block locations
+ * <li>giving Hadoop location hints where to assign a consecutive bunch of lines to
+ * </ol>
  *
- * The NLineInputFormat can be used in such applications, that splits
- * the input file such that by default, one line is fed as
- * a value to one map task, and key is the offset.
- * i.e. (k,v) is (LongWritable, Text).
- * The location hints will span the whole mapred cluster.
  */
 @InterfaceAudience.Public
 @InterfaceStability.Stable
 public class ControlFileInputFormat extends NLineInputFormat {
     private static Log LOG = LogFactory.getLog(ControlFileInputFormat.class);
-    public static final String LINES_PER_MAP = "mapreduce.input.lineinputformat.linespermap";
 
-    public RecordReader<LongWritable, Text> createRecordReader(
-            InputSplit genericSplit, TaskAttemptContext context)
-            throws IOException {
-        context.setStatus(genericSplit.toString());
-        return new LineRecordReader();
-    }
-
-    /**
-     * Rearranges the lines of the control files according to the location
-     * the input file references and logically splits the rearranged control file 
-     * into splits of about N lines.
-     *
+    /** 
+     * Logically splits the set of input files for the job, splits N lines
+     * of the input as one split.
+     * 
      * @see NLineInputFormat#getSplits(JobContext)
      */
-    public List<InputSplit> getSplits(JobContext job) throws IOException {
+    public List<InputSplit> getSplits(JobContext job)
+        throws IOException {
         List<InputSplit> splits = new ArrayList<InputSplit>();
         int numLinesPerSplit = getNumLinesPerSplit(job);
         for (FileStatus status : listStatus(job)) {
-            splits.addAll(getSplitsForFile(status, job.getConfiguration(),
-                    numLinesPerSplit));
+            splits.addAll(getSplitsForFile(status,
+                        job.getConfiguration(), numLinesPerSplit));
         }
         return splits;
     }
 
+    /**
+     * Gets the rearranged splits for a control file.
+     *
+     * Rearranges the lines of a control file according to the location
+     * the input file references and logically splits the rearranged control file
+     * into splits of about N lines.
+     * @param status
+     * @param conf
+     * @param numLinesPerSplit
+     * @return
+     * @throws IOException
+     */
     public static List<FileSplit> getSplitsForFile(FileStatus status,
             Configuration conf, int numLinesPerSplit) throws IOException {
         List<FileSplit> splits = new ArrayList<FileSplit>();
-        Path fileName = status.getPath();
-        LOG.debug("fileName = " + fileName.toString());
-        if (status.isDir()) {
-            throw new IOException("Not a file: " + fileName);
+        Path controlFile = status.getPath();
+        if (status.isDirectory()) {
+            throw new IOException("Not a file: " + controlFile);
         }
-        FileSystem fs = fileName.getFileSystem(conf);
+        FileSystem fs = controlFile.getFileSystem(conf);
         LineReader lr = null;
         try {
             CmdLineParser parser = new PipedArgsParser();
@@ -131,14 +126,15 @@ public class ControlFileInputFormat extends NLineInputFormat {
             Path fRepo = new Path(strRepo);
             Repository repo = new ToolRepository(fs, fRepo);
 
-            Map<String, ArrayList<String>> locationMap = createLocationMap(fileName, conf, repo, parser);
+            LOG.info("Creating location-aware control file");
+            Map<String, ArrayList<String>> locationMap = createLocationMap(controlFile, conf, repo, parser);
 
-            Path newControlFile = new Path(fileName + "-rearranged"
+            Path newControlFile = new Path(controlFile + "-rearranged"
                     + System.currentTimeMillis());
-            LOG.debug("newControlFile = " + newControlFile.toString());
 
             splits = writeNewControlFileAndCreateSplits(newControlFile, fs, locationMap,
                     numLinesPerSplit);
+            LOG.info("Location-aware control file " + newControlFile.toString() + " created");
 
         } finally {
             if (lr != null) {
@@ -148,6 +144,19 @@ public class ControlFileInputFormat extends NLineInputFormat {
         return splits;
     }
 
+    /**
+     * Loops over locationMap, writes control lines from locationmap
+     * to new control file and creates equally sized splits of
+     * approximate numLinesPerSplit.
+     *
+     * @param newControlFile Path to the reordered control file
+     * @param fs Hadoop filesystem handle
+     * @param locationMap map of control line -> arraylist of locations
+     * @param numLinesPerSplit approximate number of lines per split
+     * @return list of splits
+     *
+     * @throws IOException
+     */
     public static List<FileSplit> writeNewControlFileAndCreateSplits(
             Path newControlFile, FileSystem fs,
             Map<String, ArrayList<String>> locationMap, int numLinesPerSplit)
@@ -155,57 +164,35 @@ public class ControlFileInputFormat extends NLineInputFormat {
         int start = 0, byteCounter = 0;
         List<FileSplit> splits = new ArrayList<FileSplit>();
         FSDataOutputStream fsout = fs.create(newControlFile);
-        LOG.debug("numLinesPerSplit = " + numLinesPerSplit);
-        for (Entry<String, ArrayList<String>> entry : locationMap
-                .entrySet()) {
+        for (Entry<String, ArrayList<String>> entry : locationMap.entrySet()) {
             String host = entry.getKey();
-            LOG.debug(" for host = " + host);
+
             ArrayList<String> lines = entry.getValue();
             int numSplits = lines.size() / numLinesPerSplit;
             int rest = lines.size() % numLinesPerSplit;
-            double restPerSplit = rest / (double)numSplits;
-            LOG.debug(" numSplits = " + numSplits);
-            LOG.debug(" rest = " + rest);
-            LOG.debug(" restPerSplit = " + restPerSplit);
-            // x = arraysize / N ... number of splits
-            // r = x - int(x) ... fraction of rest
-            // b = (int)(N*r) ... number of rest
-            // a = b/int(x) ... number of lines per split to add
-            // i = 0; j = 0; start = 0
 
-            // if arraysize < N
             if (lines.size() <= numLinesPerSplit) {
-                LOG.debug("arraysize < N ");
                 //   put all lines into newControlFile and use the whole as a split
-                LOG.debug("writing all lines to newControlFile");
                 for (String line : lines) {
-                    LOG.debug("line = " + line);
-                    fsout.write((line+"\n").getBytes()); // FIXME : add a line break?
+                    fsout.write((line+"\n").getBytes()); 
                     byteCounter += line.length()+1;
                 }
+                // create the split and provide a location hint
                 splits.add(new FileSplit(newControlFile, start,
                         byteCounter-start, new String[] { host }));
-                LOG.debug("split created, start = " + start + ", end = "
-                        + (byteCounter-start) + ", host = " + host);
                 start = byteCounter;
             } else {
-                LOG.debug("arraysize > N");
                 int i = 1;
                 int j = rest;
                 for (String line : lines) {
-                    LOG.debug("i = " + i + ", j = " + j );
-                    LOG.debug("writing line to newControlFile");
-                    LOG.debug("line = " + line);
                     fsout.write((line+"\n").getBytes());
                     byteCounter += line.length()+1;
                     if (i < numLinesPerSplit + (j/numSplits)) {
                         i++;
                     } else {
+                        // create the split and provide a location hint
                         splits.add(new FileSplit(newControlFile, start,
                                 (byteCounter-start), new String[] { host }));
-                        LOG.debug("split created, start = " + start
-                                + ", length = " + (byteCounter-start) + ", host = "
-                                + host);
                         start = byteCounter;
                         if ( j >= numSplits ) {
                             j = numSplits % rest;
@@ -220,11 +207,22 @@ public class ControlFileInputFormat extends NLineInputFormat {
         return splits;
     }
 
+    /**
+     * Creates mapping of locations to arraylists of control lines.
+     *
+     * @param controlFile input control file
+     * @param conf Hadoop configuration
+     * @param repo Toolspec repository
+     * @param parser parser for the control lines
+     * @return mapping
+     *
+     * @throws IOException
+     */
     public static Map<String, ArrayList<String>> createLocationMap(
-            Path fileName, Configuration conf, Repository repo,
+            Path controlFile, Configuration conf, Repository repo,
             CmdLineParser parser) throws IOException {
-        FileSystem fs = fileName.getFileSystem(conf);
-        FSDataInputStream in = fs.open(fileName);
+        FileSystem fs = controlFile.getFileSystem(conf);
+        FSDataInputStream in = fs.open(controlFile);
         LineReader lr = new LineReader(in, conf);
         Text controlLine = new Text();
         Map<String, ArrayList<String>> locationMap = new HashMap<String, ArrayList<String>>();
@@ -232,14 +230,12 @@ public class ControlFileInputFormat extends NLineInputFormat {
         int l = 0;
         while ((lr.readLine(controlLine)) > 0) {
             l += 1;
-            LOG.debug("l = " + l);
             // read line by line
             Path[] inFiles = getInputFiles(fs, parser, repo, controlLine.toString());
 
             // count for each host how many blocks it holds of the current control line's input files
             String[] hostsOfFile = getSortedHosts(fs, inFiles);
 
-            String outputHosts = "";
             for ( String host : hostsOfFile ) {
                 if( !allHosts.contains(host)) allHosts.add(host);
             }
@@ -253,33 +249,33 @@ public class ControlFileInputFormat extends NLineInputFormat {
             for ( String host : hostsOfFile ) {
                 hosts[h] = hostsOfFile[h];
                 h++;
-                outputHosts += host + " ";
             }
             for( String host : theseHosts ) {
                 hosts[h] = theseHosts.get(h-hostsOfFile.length);
                 h++;
-                outputHosts += host + " ";
             }
             
-            LOG.debug("sorted hosts: " + outputHosts);
-
             addToLocationMap(locationMap, hosts, controlLine.toString(), l);
         }
         return locationMap;
     }
 
+    /**
+     * Adds control line to the locationmap and keeps locations balanced.
+     *
+     * @param locationMap mapping of locations to arraylists of control lines
+     * @param hosts locations of the control line
+     * @param line control line
+     * @param lineNum current line number of original control file
+     */
     public static void addToLocationMap(
         Map<String, ArrayList<String>> locationMap, String[] hosts,
         String line, int lineNum) {
-        LOG.debug("hosts.size = " + hosts.length);
         for (String host : hosts) {
-            LOG.debug("  host = " + host);
             ArrayList<String> lines = locationMap.containsKey(host) ? locationMap
                     .get(host) : new ArrayList<String>();
             // if the location is unbalanced (got too few lines) add line for that location
-            LOG.debug("  lines.size = " + lines.size());
             if (lines.size() < (float)lineNum / hosts.length ) {
-                LOG.debug("  host is unbalanced, adding line to it");
                 lines.add(line);
                 locationMap.put(host, lines);
                 break;
@@ -287,6 +283,18 @@ public class ControlFileInputFormat extends NLineInputFormat {
         }
     }
 
+    /**
+     * Finds input file references in the control line by looking 
+     * into its toolspec.
+     *
+     * @param fs Hadoop filesystem handle
+     * @param parser for parsing the control line
+     * @param repo Toolspec repository
+     * @param controlLine 
+     * @return array of paths to input file references
+     *
+     * @throws IOException
+     */
     public static Path[] getInputFiles(FileSystem fs, CmdLineParser parser,
             Repository repo, String controlLine) throws IOException {
         parser.parse(controlLine);
@@ -320,7 +328,6 @@ public class ControlFileInputFormat extends NLineInputFormat {
 
         // TODO traverse directories
         for (String fileRef : mapInputFileParameters.values()) {
-            LOG.debug("fileRef = " + fileRef );
             Path p = new Path(fileRef);
             if (fs.exists(p)) {
                 inFiles[i++] = p;
@@ -329,18 +336,25 @@ public class ControlFileInputFormat extends NLineInputFormat {
         return inFiles;
     }
 
+    /**
+     * Gets block locations of input files sorted
+     * by the total number of occurrences.
+     *
+     * @param fs Hadoop filesystem handle
+     * @param inFiles array of input files
+     * @return sorted String array
+     *
+     * @throws IOException
+     */
     public static String[] getSortedHosts(FileSystem fs, Path[] inFiles)
             throws IOException {
         final Map<String, Integer> hostMap = new HashMap<String, Integer>();
         for( Path inFile : inFiles ) {
-            LOG.debug("inFile = " + inFile.toString());
             FileStatus s = fs.getFileStatus(inFile);
             BlockLocation[] locations = fs.getFileBlockLocations(s, (long)0, s.getLen());
             for( BlockLocation location : locations ) {
                 String[] hosts = location.getHosts();
-                LOG.debug("  one blockLocation on: ");
                 for( String host : hosts ) {
-                    LOG.debug("    host = " + host);
                     if( !hostMap.containsKey(host) ) {
                         hostMap.put(host, 1);
                         continue;
@@ -360,5 +374,6 @@ public class ControlFileInputFormat extends NLineInputFormat {
         return hosts.toArray(new String[0]);
 
     }
+
 }
 
