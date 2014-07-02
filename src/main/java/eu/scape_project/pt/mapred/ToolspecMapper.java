4ef6308840f56283ed490595bcee8479431a1215
Alastair Duncan <alastair.duncan@stfc.ac.uk>
2014-07-02 15:36:28 +0100

    glob added for wildcard input files

--- src/main/java/eu/scape_project/pt/mapred/ToolspecMapper.java
+++ src/main/java/eu/scape_project/pt/mapred/ToolspecMapper.java
@@ -27,6 +27,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -34,227 +35,240 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 /**
  * The Toolspec executor.
- *
+ * 
  * @author Rainer Schmidt [rschmidt13]
  * @author Matthias Rella [myrho]
  */
 public class ToolspecMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
 
-    private final Log LOG = LogFactory.getLog(getClass());
-    private static final String SEP = " ";
+	private final Log LOG = LogFactory.getLog(getClass());
+	private static final String SEP = " ";
 
-    private CmdLineParser parser;
-    private Repository repo;
-    private Tool tool;
-    private Operation operation;
-    private Configuration conf;
+	private CmdLineParser parser;
+	private Repository repo;
+	private Tool tool;
+	private Operation operation;
+	private Configuration conf;
+	private FileSystem fs;
 
-    /**
-     * Sets up toolspec repository and parser.
-     */
-    @Override
-    public void setup(Context context) throws IOException {
-        conf = context.getConfiguration();
-        String strRepo = conf.get(PropertyNames.REPO_LOCATION).trim();
-        
-        Path fRepo = new Path(strRepo);
-        
-        FileSystem fs = FileSystem.get(conf);
-        
-        //this.repo = new ToolRepository(fs, fRepo);
-        this.repo = new ToolRepository(conf, fRepo);
+	/**
+	 * Sets up toolspec repository and parser.
+	 */
+	@Override
+	public void setup(Context context) throws IOException {
+		conf = context.getConfiguration();
+		String strRepo = conf.get(PropertyNames.REPO_LOCATION).trim();
 
-        // create parser of command line input arguments
-        parser = new PipedArgsParser();
-    }
+		Path fRepo = new Path(strRepo);
 
-    /**
-     * The map gets a key and value, the latter being a line describing
-     * stdin and stdout file references and (a pipe of) tool/action pair(s) with
-     * parameters.
-     *
-     * 1. Parse the input command-line and read parameters and arguments.
-     * 2. Localize input and output file references and input stream.
-     * 3. Chain piped commands to a simple-chained list of processors and run them.
-     * 4. "De-localize" output files and output streams
-     *
-     * @param key offset in control file
-     * @param value line describing the (piped) command(s) and stdin/out file refs
-     * @param context Job context
-     */
-    @Override
-    public void map(LongWritable key, Text value, Context context ) throws IOException {
-        LOG.info("Mapper.map key:" + key.toString() + " value:" + value.toString());
+		fs = FileSystem.get(conf);
 
-        Text text = null;
-        try {
-            // parse input line for stdin/out file refs and tool/action commands
-            parser.parse(value.toString());
+		// this.repo = new ToolRepository(fs, fRepo);
+		this.repo = new ToolRepository(conf, fRepo);
 
-            final Command[] commands = parser.getCommands();
-            final String strStdinFile = parser.getStdinFile();
-            final String strStdoutFile = parser.getStdoutFile();
+		// create parser of command line input arguments
+		parser = new PipedArgsParser();
+	}
 
-            Processor firstProcessor = null;
-            ToolProcessor lastProcessor = null; 
+	/**
+	 * The map gets a key and value, the latter being a line describing stdin
+	 * and stdout file references and (a pipe of) tool/action pair(s) with
+	 * parameters.
+	 * 
+	 * 1. Parse the input command-line and read parameters and arguments. 2.
+	 * Localize input and output file references and input stream. 3. Chain
+	 * piped commands to a simple-chained list of processors and run them. 4.
+	 * "De-localize" output files and output streams
+	 * 
+	 * @param key
+	 *            offset in control file
+	 * @param value
+	 *            line describing the (piped) command(s) and stdin/out file refs
+	 * @param context
+	 *            Job context
+	 */
+	@Override
+	public void map(LongWritable key, Text value, Context context) throws IOException {
+		LOG.info("Mapper.map key:" + key.toString() + " value:" + value.toString());
 
-            Map<String, String>[] mapOutputFileParameters = new HashMap[commands.length];
+		Text text = null;
+		try {
+			// parse input line for stdin/out file refs and tool/action commands
+			parser.parse(value.toString());
 
-            for(int c = 0; c < commands.length; c++ ) {
-                Command command = commands[c];
+			final Command[] commands = parser.getCommands();
+			final String strStdinFile = parser.getStdinFile();
+			final String strStdoutFile = parser.getStdoutFile();
 
-                this.tool = repo.getTool(command.getTool());
+			Processor firstProcessor = null;
+			ToolProcessor lastProcessor = null;
 
-                lastProcessor = new ToolProcessor(this.tool);
+			Map<String, String>[] mapOutputFileParameters = new HashMap[commands.length];
 
-                this.operation = lastProcessor.findOperation(command.getAction());
-                if( this.operation == null )
-                    throw new IOException(
-                            "operation " + command.getAction() + " not found");
+			for (int c = 0; c < commands.length; c++) {
+				Command command = commands[c];
 
-                lastProcessor.setOperation(this.operation);
+				this.tool = repo.getTool(command.getTool());
 
-                lastProcessor.initialize();
+				lastProcessor = new ToolProcessor(this.tool);
 
-                lastProcessor.setParameters(command.getPairs());
-                lastProcessor.setWorkingDir(workingDir());
+				this.operation = lastProcessor.findOperation(command.getAction());
+				if (this.operation == null)
+					throw new IOException("operation " + command.getAction() + " not found");
 
-                // get parameters accepted by the lastProcessor.
-                Map<String, String> mapInputFileParameters = lastProcessor.getInputFileParameters(); 
-                mapOutputFileParameters[c] = lastProcessor.getOutputFileParameters(); 
+				lastProcessor.setOperation(this.operation);
 
-                // copy parameters to temporal map
-                Map<String, String> mapTempInputFileParameters = 
-                    new HashMap<String, String>(mapInputFileParameters);
+				lastProcessor.initialize();
 
-                // localize parameters
-                for( Entry<String, String> entry : mapInputFileParameters.entrySet()) {
-                    LOG.debug("input = " + entry.getValue());
-                    String localFileRefs = localiseFileRefs(entry.getValue().trim());
-                    mapTempInputFileParameters.put( entry.getKey(), localFileRefs.substring(1));
-                }
+				lastProcessor.setParameters(command.getPairs());
+				lastProcessor.setWorkingDir(workingDir());
 
-                Map<String, String> mapTempOutputFileParameters = 
-                        new HashMap<String, String>(mapOutputFileParameters[c]);
-                for( Entry<String, String> entry : mapOutputFileParameters[c].entrySet()) {
-                    LOG.debug("output = " + entry.getValue());
-                    String localFileRefs = localiseFileRefs(entry.getValue().trim());
-                    mapTempOutputFileParameters.put( entry.getKey(), localFileRefs.substring(1));
-                }
+				// get parameters accepted by the lastProcessor.
+				Map<String, String> mapInputFileParameters = lastProcessor.getInputFileParameters();
+				mapOutputFileParameters[c] = lastProcessor.getOutputFileParameters();
 
-                // feed processor with localized parameters
-                lastProcessor.setInputFileParameters(mapTempInputFileParameters);
-                lastProcessor.setOutputFileParameters(mapTempOutputFileParameters);
+				// copy parameters to temporal map
+				Map<String, String> mapTempInputFileParameters = new HashMap<String, String>(mapInputFileParameters);
 
-                // chain processor
-                if(firstProcessor == null )
-                    firstProcessor = lastProcessor;
-                else {
-                    Processor help = firstProcessor;  
-                    while(help.next() != null ) help = help.next();
-                    help.next(lastProcessor);
-                }
-            }
+				// localize parameters
+				for (Entry<String, String> entry : mapInputFileParameters.entrySet()) {
+					LOG.debug("input = " + entry.getValue());
+					String localFileRefs = localiseFileRefs(entry.getValue().trim());
+					mapTempInputFileParameters.put(entry.getKey(), localFileRefs.substring(1));
+				}
 
-            // Processors for stdin and stdout
-            StreamProcessor streamProcessorIn = createStreamProcessorIn(strStdinFile);
-            if( streamProcessorIn != null ) {
-                streamProcessorIn.next(firstProcessor);
-                firstProcessor = streamProcessorIn;
-            } 
+				Map<String, String> mapTempOutputFileParameters = new HashMap<String, String>(mapOutputFileParameters[c]);
+				for (Entry<String, String> entry : mapOutputFileParameters[c].entrySet()) {
+					LOG.debug("output = " + entry.getValue());
+					String localFileRefs = localiseFileRefs(entry.getValue().trim());
+					mapTempOutputFileParameters.put(entry.getKey(), localFileRefs.substring(1));
+				}
 
-            OutputStream oStdout = createStdOut(strStdoutFile);
-            StreamProcessor streamProcessorOut = new StreamProcessor(oStdout);
-            lastProcessor.next(streamProcessorOut);
-            
-            firstProcessor.execute();
+				// feed processor with localized parameters
+				lastProcessor.setInputFileParameters(mapTempInputFileParameters);
+				lastProcessor.setOutputFileParameters(mapTempOutputFileParameters);
 
-            delocalizeOutputParameters(mapOutputFileParameters);
+				// chain processor
+				if (firstProcessor == null)
+					firstProcessor = lastProcessor;
+				else {
+					Processor help = firstProcessor;
+					while (help.next() != null)
+						help = help.next();
+					help.next(lastProcessor);
+				}
+			}
 
-            text = convertToResult(oStdout, strStdoutFile);
+			// Processors for stdin and stdout
+			StreamProcessor streamProcessorIn = createStreamProcessorIn(strStdinFile);
+			if (streamProcessorIn != null) {
+				streamProcessorIn.next(firstProcessor);
+				firstProcessor = streamProcessorIn;
+			}
+			OutputStream oStdout = createStdOut(strStdoutFile);
+			StreamProcessor streamProcessorOut = new StreamProcessor(oStdout);
+			lastProcessor.next(streamProcessorOut);
 
-        } catch (Exception ex) {
-            LOG.error("error during mapping", ex);
-            text = convertToResult(ex);
-        } finally {
-            writeMappingResult(key, text, context);
-        }
-    }
+			firstProcessor.execute();
 
-    private String localiseFileRefs(String localFile) throws IOException {
-        String[] remoteFileRefs = localFile.split(SEP);
-        String localFileRefs = "";
-        String workingDir = workingDir();
-        for( int i = 0; i < remoteFileRefs.length; i++ ){
-            Filer filer = Filer.create(remoteFileRefs[i]);
-            filer.setWorkingDir(workingDir);
-            filer.localize();
-            localFileRefs = localFileRefs + SEP + filer.getRelativeFileRef();
-        }
-        return localFileRefs;
-    }
+			delocalizeOutputParameters(mapOutputFileParameters);
 
-    private Text convertToResult(OutputStream oStdout, final String strStdoutFile) {
-    	System.out.println("convertToResult(OutputStream oStdout");
-        if( oStdout instanceof ByteArrayOutputStream )
-            return  new Text( ((ByteArrayOutputStream)oStdout).toByteArray() );
-        return new Text( strStdoutFile );
-    }
+			text = convertToResult(oStdout, strStdoutFile);
 
-    private Text convertToResult(Exception ex) {
-    	
-        StringWriter writer = new StringWriter();
-        ex.printStackTrace(new PrintWriter(writer));
-        System.out.println("convertToResult(Exception ex): " +  writer.toString());
-        return new Text( "ERROR: " + writer.toString() );
-    }
+		} catch (Exception ex) {
+			LOG.error("error during mapping", ex);
+			text = convertToResult(ex);
+		} finally {
+			writeMappingResult(key, text, context);
+		}
+	}
 
-    private OutputStream createStdOut(final String strStdoutFile) throws IOException {
-        if( strStdoutFile != null ) 
-            return Filer.create(strStdoutFile).getOutputStream();
-        // default: output to bytestream
-        return new ByteArrayOutputStream();
-    }
+	private String localiseFileRefs(String localFile) throws IOException {
 
-    private StreamProcessor createStreamProcessorIn(final String strStdinFile) throws IOException {
-        if( strStdinFile != null ) {
-            InputStream iStdin = Filer.create(strStdinFile).getInputStream();
-            return new StreamProcessor(iStdin);
-        }
-        return null;
-    }
+		String[] remoteFileRefs = localFile.split(SEP);
+		String localFileRefs = "";
+		String workingDir = workingDir();
+		LOG.debug("localiseFileRefs: localFile: " + localFile);
+		for (int i = 0; i < remoteFileRefs.length; i++) {
+			LOG.debug("localiseFileRefs: remote file Ref: " + remoteFileRefs[i]);
+			FileStatus[] files = fs.globStatus(new Path(remoteFileRefs[i]));
+			if (files != null) {
+				for (int n = 0; n < files.length; n++) {
+					Filer filer = Filer.create(files[n].getPath().toUri().toString());
+					LOG.debug("localiseFileRefs: glob: " + files[n].getPath().toUri().toString());
+					filer.setWorkingDir(workingDir);
+					filer.localize();
+					localFileRefs = localFileRefs + SEP + filer.getRelativeFileRef();
+				}
+			}else{
+				Filer filer = Filer.create(remoteFileRefs[i]);
+				LOG.debug("localiseFileRefs: remote file Ref single file: " + remoteFileRefs[i]);
+				filer.setWorkingDir(workingDir);
+				filer.localize();
+				localFileRefs = localFileRefs + SEP + filer.getRelativeFileRef();
+			}
+		}
+		return localFileRefs;
+	}
 
-    private void delocalizeOutputParameters(Map<String, String>[] mapOutputFileParameters) throws IOException {
-        for(int i = 0; i < mapOutputFileParameters.length; i++ ) {
-            Map<String, String> outputFileParameters = mapOutputFileParameters[i];
-            delocalizeOutputParameters(outputFileParameters);
-        }
-    }
+	private Text convertToResult(OutputStream oStdout, final String strStdoutFile) {
+		
+		if (oStdout instanceof ByteArrayOutputStream)
+			return new Text(((ByteArrayOutputStream) oStdout).toByteArray());
+		return new Text(strStdoutFile);
+	}
 
-    private void delocalizeOutputParameters(Map<String, String> outputFileParameters) throws IOException {
-        String workingDir = workingDir();
-        for( String strFile : outputFileParameters.values())
-        {
-            String[] localFileRefs = strFile.split(SEP);
-            for( int j = 0; j < localFileRefs.length; j++ ){
-                Filer filer = Filer.create(localFileRefs[j]);
-                filer.setWorkingDir(workingDir);
-                filer.delocalize();
-            }
-        }
-    }
+	private Text convertToResult(Exception ex) {
 
-    private String workingDir() {
-    	System.out.println("workingDir: " + System.getProperty("user.dir"));
-        return System.getProperty("user.dir");
-    }
+		StringWriter writer = new StringWriter();
+		ex.printStackTrace(new PrintWriter(writer));
+		return new Text("ERROR: " + writer.toString());
+	}
 
-    private void writeMappingResult(LongWritable key, Text text, Context context) throws IOException {
-        try {
-            context.write( key, text);
-        } catch (InterruptedException ex) {
-            LOG.error("error during write", ex);
-            throw new IOException(ex);
-        }
-    }
+	private OutputStream createStdOut(final String strStdoutFile) throws IOException {
+		if (strStdoutFile != null)
+			return Filer.create(strStdoutFile).getOutputStream();
+		// default: output to bytestream
+		return new ByteArrayOutputStream();
+	}
+
+	private StreamProcessor createStreamProcessorIn(final String strStdinFile) throws IOException {
+		if (strStdinFile != null) {
+			InputStream iStdin = Filer.create(strStdinFile).getInputStream();
+			return new StreamProcessor(iStdin);
+		}
+		return null;
+	}
+
+	private void delocalizeOutputParameters(Map<String, String>[] mapOutputFileParameters) throws IOException {
+		for (int i = 0; i < mapOutputFileParameters.length; i++) {
+			Map<String, String> outputFileParameters = mapOutputFileParameters[i];
+			delocalizeOutputParameters(outputFileParameters);
+		}
+	}
+
+	private void delocalizeOutputParameters(Map<String, String> outputFileParameters) throws IOException {
+		String workingDir = workingDir();
+		for (String strFile : outputFileParameters.values()) {
+			String[] localFileRefs = strFile.split(SEP);
+			for (int j = 0; j < localFileRefs.length; j++) {
+				Filer filer = Filer.create(localFileRefs[j]);
+				filer.setWorkingDir(workingDir);
+				filer.delocalize();
+			}
+		}
+	}
+
+	private String workingDir() {
+		return System.getProperty("user.dir");
+	}
+
+	private void writeMappingResult(LongWritable key, Text text, Context context) throws IOException {
+		try {
+			context.write(key, text);
+		} catch (InterruptedException ex) {
+			LOG.error("error during write", ex);
+			throw new IOException(ex);
+		}
+	}
 }
--- src/main/java/eu/scape_project/pt/util/fs/HDFSFiler.java
+++ src/main/java/eu/scape_project/pt/util/fs/HDFSFiler.java
@@ -10,148 +10,149 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
 
 /**
- * Handles the transportation of files from the local filesystem to HDFS 
- * and vice-versa.
+ * Handles the transportation of files from the local filesystem to HDFS and
+ * vice-versa.
  * 
  * @author Rainer Schmidt [rschmidt13]
  * @author Matthias Rella [myrho]
  * @author Martin Schenck [schenck]
  */
 public class HDFSFiler extends Filer {
-    
-    private static Log LOG = LogFactory.getLog(HDFSFiler.class);
-    
-    /**
-     * Hadoop Filesystem handle.
-     */
-    private final FileSystem hdfs;
 
-    /**
-     * File to handle by this filer
-     */
-    private final Path file;
+	private static Log LOG = LogFactory.getLog(HDFSFiler.class);
 
-    HDFSFiler(URI uri) throws IOException {
-        this.file = new Path(uri);
-        hdfs = file.getFileSystem(new Configuration());
-    }
-    
-    /**
-     * Copies a file or directory from the local filesystem to a remote one.
-     */
-    private void depositDirectoryOrFile(String strSrc, String strDest) throws IOException {
-        File source = new File( strSrc );
-        if(source.isDirectory()) {
-            depositDirectory(strSrc, strDest);
-        } else {
-            depositFile(strSrc, strDest);
-        }
-    }
-    
-    /**
-     * Copies a directory from the local filesystem to a remote one.
-     */
-    private void depositDirectory(String strSrc, String strDest) throws IOException {
-        // Get output directory name from strSrc
-        File localDir = new File( strSrc );
-        
-        if(!localDir.isDirectory()) {
-            throw new IOException("Could not find correct local output directory: " + localDir );
-        }
-        
-        LOG.debug("Local directory is: " + localDir );
-        
-        for(File localFile : localDir.listFiles()) {
-            depositDirectoryOrFile(localFile.getCanonicalPath(), strDest + File.separator + localFile.getName());
-        }
-    }
+	/**
+	 * Hadoop Filesystem handle.
+	 */
+	private final FileSystem hdfs;
 
-    /**
-     * Copies a file from the local filesystem to a remote one.
-     */
-    private void depositFile(String strSrc, String strDest) throws IOException {
-        Path src = new Path(strSrc);
-        Path dest = new Path(strDest);
-        
-        LOG.debug("local file name is: "+src+" destination path is:" +dest);
-        hdfs.copyFromLocalFile(src, dest);
-    }
+	/**
+	 * File to handle by this filer
+	 */
+	private final Path file;
 
-    @Override
-    public void localize() throws IOException {
-        File fileRef = new File(getAbsoluteFileRef());
-        LOG.debug("localize " + fileRef);
-        new File(fileRef.getParent()).mkdirs();
-        Path localfile = new Path( fileRef.toString() );
-        if(hdfs.exists(file)) {
-            hdfs.copyToLocalFile(file, localfile);
-        }
-    }
+	HDFSFiler(URI uri) throws IOException {
+		this.file = new Path(uri);
+		hdfs = file.getFileSystem(new Configuration());
+	}
 
-    @Override
-    public void delocalize() throws IOException {
-        this.depositDirectoryOrFile(getAbsoluteFileRef(), file.toString());
-    }
+	/**
+	 * Copies a file or directory from the local filesystem to a remote one.
+	 */
+	private void depositDirectoryOrFile(String strSrc, String strDest) throws IOException {
+		File source = new File(strSrc);
+		if (source.isDirectory()) {
+			depositDirectory(strSrc, strDest);
+		} else {
+			depositFile(strSrc, strDest);
+		}
+	}
 
-    @Override
-    public void setWorkingDir(String strDir ) {
-        LOG.debug("setDirectory " + strDir );
-        File dirFile = new File(strDir);
-        if( !dirFile.isAbsolute() ) {
-            this.dir = this.getTmpDir() + strDir;
-        } else {
-            this.dir = strDir;
-        }
-        LOG.debug("this.dir = " + this.dir );
-    }
+	/**
+	 * Copies a directory from the local filesystem to a remote one.
+	 */
+	private void depositDirectory(String strSrc, String strDest) throws IOException {
+		// Get output directory name from strSrc
+		File localDir = new File(strSrc);
 
-    @Override
-    public String getAbsoluteFileRef() {
-        return this.getFullDirectory();
-    }
+		if (!localDir.isDirectory()) {
+			throw new IOException("Could not find correct local output directory: " + localDir);
+		}
 
-    @Override
-    public String getRelativeFileRef() {
-        String path = this.getPath();
-        if( path.startsWith(System.getProperty("file.separator")) )
-            path = path.substring(1);
-        return path;
-    }
+		LOG.info("depositDirectory Local directory is: " + localDir);
 
-    /**
-     * Returns the user defined directory of the file.
-     */
-    private String getPath() {
-        URI uri = this.file.toUri();
-        
-        String path = uri.getPath();
-        LOG.debug("path = " + path);
-        String sep = System.getProperty("file.separator");
-        return path.replace(Path.SEPARATOR, sep);
-    }
+		for (File localFile : localDir.listFiles()) {
+			depositDirectoryOrFile(localFile.getCanonicalPath(), strDest + File.separator + localFile.getName());
+		}
+	}
 
-    /**
-     * Returns working space directory with user defined directories.
-     */
-    private String getFullDirectory() {
-        String par = this.getPath();
-        return (this.dir.isEmpty() 
-                ? "hdfsfiler_" + file.hashCode()
-                : this.dir) 
-                + par;
-    }
+	/**
+	 * Copies a file from the local filesystem to a remote one.
+	 */
+	private void depositFile(String strSrc, String strDest) throws IOException {
 
-    @Override
-    public InputStream getInputStream() throws IOException {
-        return hdfs.open(file);
-    }
+		Path src = new Path(strSrc);
+		Path dest = new Path(strDest);
+		LOG.info("depositFile: local file name is: " + src + " destination path is:" + dest);
+		FileStatus[] files = hdfs.globStatus(src);
+		if (files != null) {
+			for (int i = 0; i < files.length; i++) {
+				hdfs.copyFromLocalFile(files[i].getPath(), dest);
+			}
+		}else{
+			hdfs.copyFromLocalFile(src, dest);
+		}
+	}
 
-    @Override
-    public OutputStream getOutputStream() throws IOException {
-        return hdfs.create(file);
-    }
+	@Override
+	public void localize() throws IOException {
+		File fileRef = new File(getAbsoluteFileRef());
+		new File(fileRef.getParent()).mkdirs();
+		Path localfile = new Path(fileRef.toString());
+		if (hdfs.exists(file)) {
+			hdfs.copyToLocalFile(file, localfile);
+		}
+	}
+
+	@Override
+	public void delocalize() throws IOException {
+		this.depositDirectoryOrFile(getAbsoluteFileRef(), file.toString());
+	}
+
+	@Override
+	public void setWorkingDir(String strDir) {
+		File dirFile = new File(strDir);
+		if (!dirFile.isAbsolute()) {
+			this.dir = this.getTmpDir() + strDir;
+		} else {
+			this.dir = strDir;
+		}
+	}
+
+	@Override
+	public String getAbsoluteFileRef() {
+		return this.getFullDirectory();
+	}
+
+	@Override
+	public String getRelativeFileRef() {
+		String path = this.getPath();
+		if (path.startsWith(System.getProperty("file.separator")))
+			path = path.substring(1);
+		return path;
+	}
+
+	/**
+	 * Returns the user defined directory of the file.
+	 */
+	private String getPath() {
+		URI uri = this.file.toUri();
+
+		String path = uri.getPath();
+		String sep = System.getProperty("file.separator");
+		return path.replace(Path.SEPARATOR, sep);
+	}
+
+	/**
+	 * Returns working space directory with user defined directories.
+	 */
+	private String getFullDirectory() {
+		String par = this.getPath();
+		return (this.dir.isEmpty() ? "hdfsfiler_" + file.hashCode() : this.dir) + par;
+	}
+
+	@Override
+	public InputStream getInputStream() throws IOException {
+		return hdfs.open(file);
+	}
+
+	@Override
+	public OutputStream getOutputStream() throws IOException {
+		return hdfs.create(file);
+	}
 
 }
